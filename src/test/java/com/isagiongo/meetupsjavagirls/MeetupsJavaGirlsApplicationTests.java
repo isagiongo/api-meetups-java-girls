5caf40ec2f5d21416b452f650e5e190c6944a1c9
Isa Giongo <isagiongo@gmail.com>
2019-09-01 20:03:55 -0300

    Remove code smell de exception

--- src/test/java/com/isagiongo/meetupsjavagirls/MeetupsJavaGirlsApplicationTests.java
+++ src/test/java/com/isagiongo/meetupsjavagirls/MeetupsJavaGirlsApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MeetupsJavaGirlsApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Test
+	public void contextLoads() {
+	}
 
 }
