289c88d9581a9af1e6a548de3afc9e7b274f153a
Isa Giongo <isagiongo@gmail.com>
2019-09-01 10:51:31 -0300

    Inicia implementacao de testes de integração

---
+++ src/test/java/com/isagiongo/meetupsjavagirls/integration/MeetupControllerIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.isagiongo.meetupsjavagirls.integration;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MeetupControllerIntegrationTest {
+
+    @LocalServerPort
+    private int randomPort;
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = randomPort;
+    }
+
+    @Test
+    public void deveRetornarOkAoBuscarTodosOsMeetups() {
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .get("/api/v1/meetups")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
